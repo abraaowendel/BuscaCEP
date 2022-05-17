b3c8de73491b7b1529685ac1108c27bc733680ed
Abraao Wendel <abraao.dev@gmail.com>
2022-05-17 20:20:09 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const searchCEP = () =>{
+    const cep = document.querySelector('#cep').value;
+    alert('A')
+}
+const search = document.querySelector('#search__cep').addEventListener('click', searchCEP)
+
+/*  <div class="modal">
+            <div class="modal__content">      
+                <input type="text" id="street" readonly placeholder="Rua">
+                <input type="text" id="district" readonly placeholder="Centro">
+                <input type="text" id="city" readonly placeholder="Cidade">
+                <input type="text" id="state" readonly placeholder="Estado">
+            </div>
+            <div id="modal__close">
+                <img src="./assets/img/close.svg" alt="">
+            </div>
+        </div> */
